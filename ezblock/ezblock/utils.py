68f7600a201e42ef186133cf1a012afb644bfb57
Cavon <cavonxx@gmail.com>
2019-04-26 15:52:33 +0800

    fix bug

---
+++ ezblock/ezblock/utils.py
@@ -0,0 +1,22 @@
+from ezblock.ble import BLE
+import os
+
+ble = BLE()
+
+ble.write('NAME+ezb-RPi')
+# ble.write('ADVP+') # 0~F
+
+__PRINT__ = print
+
+def print(msg, end='\n', tag='[DEBUG]'):
+    _msg = "EZblock [{}] [DEBUG] {}".format(time.asctime(), msg)
+    os.system("echo {} >> /opt/ezblock/log".format(_msg))
+    msg = '%s %s %s' % (tag, msg, tag)
+    __PRINT__(msg, end=end)
+    ble.write(msg)
+
+def delay(ms):
+    sleep(ms/1000)
+
+def mapping(x, in_min, in_max, out_min, out_max):
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
