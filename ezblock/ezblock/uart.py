606dabff4b03ef785c548b924fe1e09a14cc1910
star <1972133159@qq.com>
2019-10-09 15:41:09 +0800

    optimization

--- ezblock/ezblock/uart.py
+++ ezblock/ezblock/uart.py
@@ -1,8 +1,8 @@
 from serial import Serial
 
 class UART(object):
-    def __init__(self, port, baudrate):
-        self._port = port
+    def __init__(self, device, baudrate=115200, tx=None, rx=None):
+        self._port = device
         self._baudrate = baudrate
         self.ser = Serial(self._port, self._baudrate, timeout=1)
 
@@ -15,17 +15,13 @@ class UART(object):
         return buf
 
     def readinto(self, buf):
-        #buf = self.ser.read(num)
-        pass
+        buf = self.ser.read(len(buf))
+        # pass
 
     def readline(self):
         pass
 
     def write(self, buf):
-        # buf = buf.encode('utf-8')
-        self.ser.write(buf)
-
-    def writechar(self, buf):
         # buf = buf.encode('utf-8')
         self.ser.write(buf)
 
