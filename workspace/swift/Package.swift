910e14fa1fca9354135297c4da02512acf1a8067
Lovmong <1967471283@qq.com>
2022-11-25 15:48:36 +0800

    remove redundancy

--- workspace/swift/Package.swift
+++
@@ -1,8 +0,0 @@
-import PackageDescription
-let package = Package(
-    name: "led-blink",
-    dependencies: [
-        .Package(url: "https://github.com/uraimo/SwiftyGPIO.git", majorVersion: 0),
-        .Package(url: "./Source/libs/pin.swift"),
-    ]
-)
