33fcc1d73224e4de4a5b1b34b9bd2d0483938625
Lovmong <1967471283@qq.com>
2022-12-01 10:19:42 +0800

    fix bug of ezb_update

--- workspace/ezb_update.py
+++ workspace/ezb_update.py
@@ -23,7 +23,7 @@ def log(msg:str=None,level='Update',end='\n',flush=False,timestamp=True):
         print('%s,%s[%s] %s'%(_time,_msecs,level,msg), end=end, flush=flush, file=sys.stdout)
     else:
         # print('%s'%msg, end=end, flush=flush, file=log_file)
-        print('%s'%msg, end=end, flush=flush, file=sys.stdout) 
+        print('%s'%msg, end=end, flush=flush, file=sys.stdout)
 
 
 class Ezbupdate(object):
@@ -36,7 +36,7 @@ class Ezbupdate(object):
     def get_status(self, _app_version:str):
         result = self.check_version(_app_version)
         if result == False:
-            return False  
+            return False
         else:
             if result[1] > 0:
                 return True
@@ -52,8 +52,8 @@ class Ezbupdate(object):
         
         # Get information from the server
         for i in range(1,6,1):
-            try:               
-                r = requests.get(self.url+'/version32.json',timeout=5)               
+            try:
+                r = requests.get(self.url+'/version32.json',timeout=5)
                 if r.status_code != 200:
                     log('Failed to connect server. Reconnect ... %s'% i)
                 else:
@@ -61,7 +61,7 @@ class Ezbupdate(object):
                     break
                 log('requests status_code: %s' % r.status_code)
             except Exception as e:
-                log('\n[Exception] requests.get(): %s\n'% e)  
+                log('\n[Exception] requests.get(): %s\n'% e)
         else:
             log('\nFailed to connect server. Please try again later.\n')
             return False
@@ -69,7 +69,7 @@ class Ezbupdate(object):
         # Read the version information returned by the server
         result = json.loads(r.text)
         version_list = result.get('version')
-        log('version_list: %s'%version_list)        
+        log('version_list: %s'%version_list)
         latest_version = version_list[0]
         log('latest version: %s'%latest_version)
         # Read the local version information in flie '/opt/ezblock/ezb-info.ini'
@@ -79,114 +79,86 @@ class Ezbupdate(object):
                 + "\nin the \'/opt/ezblock/ezb-info.ini\' file"
                 + "\n [message] \n version =  \n ")
             return False
-        else: 
+        else:
             local_version = version_message['version']
             log('local version: %s'% local_version)
             log('app version: %s'% _app_version)
-        # Determine how many versions are different based on the position of the version in the column
-        try:
-            index = 0
-            
-            if local_version in version_list:
-                index = version_list.index(local_version)
-            # If the local version is not in the version list, find out where the version should be in the list by numerical comparison
-            else:
-                # In python, you can directly use strings to compare the version size
-                for _compare_version in version_list:
-                    if _compare_version[0] == local_version:
-                        pass
-                    elif _compare_version[0] > local_version:
-                        index += 1
-                    else:
-                        break
 
-                # _local_version = local_version.split('.')
-                # print("local version : %s %s %s "%(_local_version[0], _local_version[1], _local_version[2]))
-                # for _compare_version in version_list:
-                #     _compare_version = _compare_version.split('.')
-                #     # comparee major version
-                #     if _compare_version[0] > _local_version[0]:
-                #         index += 1
-                #     elif _compare_version[0] < _local_version[0]:
-                #         break
-                #     # comparee minor version
-                #     else:
-                #         if _compare_version[1] > _local_version[1]:
-                #             index += 1
-                #         elif _compare_version[1] < _local_version[1]:
-                #             break
-                #         # comparee micro version
-                #         else:
-                #             if _compare_version[2] > _local_version[2]:
-                #                 index += 1
-                #             elif _compare_version[2] < _local_version[2]:
-                #                 break
+        try:
+            # Determine how many versions are different based on the position of the version in the column
+            index = 0
+            updatable_version_list = []
+
+            # In python, you can directly use strings to compare the version size
+            for _compare_version in version_list:
+                if _compare_version[0] > local_version:
+                        index += 1
+                else:
+                    break
+
+            if index == 0:
+                log('\n\tAlready the latest version.')
+                return None, 0
+            else:
+                for i in range(index-1,-1,-1):
+                    if version_list[i][1] <= _app_version:
+                        updatable_version_list.append(version_list[i][0])
+                    else:
+                        log("minimum compatible version: %s"%_compare_version[1])
+                        log("\033[1;35m%s\033[0m"%"There are some updates that are not compatible with the app version.")
+                        break
+                log("updatable_version_list: %s"%updatable_version_list)
+                return updatable_version_list, len(updatable_version_list)
+
         except Exception as e:
-            log("Incorrect version information in the \'/opt/ezblock/ezb-info.ini\' file.")
             log('\033[1;35mlog: \033[0m\n\t%s'% e)
             return False
 
-        if index == 0:
-            log('\n\tAlready the latest version.')    
-            return list([0]), index  
-        else:
-            # Compare the compatibility of the updated package with the current app version
-            log("minimum compatible version: %s"%version_list[index-1][1])
-            if _app_version >= version_list[index-1][1]:
-                imgage_version_list = []
-                for v in version_list:
-                    imgage_version_list.append(v[0])
-                log("imgage_version_list: %s"%imgage_version_list)
-                return list(imgage_version_list), index
-            else:
-                log("\n\tThe app is not compatible with the update package, please upgrade the app.")
-                return list([0]), 0
-
     def update(self, _app_version:str):
         # check version return list of all versions and the index of local version in the list
-        imgage_version_list, index = self.check_version(_app_version)
+        updatable_version_list, index = self.check_version(_app_version)
 
         if index == 0:
-            return False      
+            return False
         else:
             # Update one by one from the old version to the new version
-            for i in range(index-1,-1,-1):
+            for i in range(index):
                 # Download
-                log("Download the %s version update package ..."% imgage_version_list[i])
-                download_cmd = "sudo wget -c " + self.url + str(imgage_version_list[i])+ ".zip" + " -O /opt/ezblock/" + str(imgage_version_list[i]) + ".zip"
+                log("Download the %s version update package ..."% updatable_version_list[i])
+                download_cmd = "sudo wget -c " + self.url + str(updatable_version_list[i])+ ".zip" + " -O /opt/ezblock/" + str(updatable_version_list[i]) + ".zip"
                 for j in range(1,6,1):
                     try:
-                        cmd_status, cmd_result = run_command(download_cmd) 
+                        cmd_status, cmd_result = run_command(download_cmd)
                         if cmd_status != 0:
                             log('Download failed.Retrying now ...  %s'% j)
                         else:
                             log('Download completed')
                             break
                     except Exception as e:
-                        log('\n[Exception] : %s\n'% e)  
+                        log('\n[Exception] : %s\n'% e)
                 else:
                     log('Download failed.Please check the network and try again later.')
-                    log('\033[1;35mlog: \033[0m\n%s'% cmd_result)
-                    run_command("sudo rm /opt/ezblock/"+str(imgage_version_list[i])+".zip")
+                    log('\033[1;35merror: \033[0m\n\t%s'% cmd_result)
+                    run_command("sudo rm /opt/ezblock/"+str(updatable_version_list[i])+".zip")
                     return False
 
                 # Unpacking
-                log('Unpacking update package ....')       
-                unzip_cmd = "sudo unzip -o /opt/ezblock/" + str(imgage_version_list[i]) + '.zip' + " -d /opt/ezblock/" 
+                log('Unpacking update package ....')
+                unzip_cmd = "sudo unzip -o /opt/ezblock/" + str(updatable_version_list[i]) + '.zip' + " -d /opt/ezblock/"
                 try:
                     cmd_status, cmd_result = run_command(unzip_cmd)
                     if cmd_status != 0:
                         log('Unpacking error.Please try again.')
                         log('\033[1;35mlog: \033[0m\n%s'% cmd_result)
-                        return False 
+                        return False
                     else:
                         log('Unzip completed')
                 except Exception as e:
-                    log('\n[Exception] : %s\n'% e)    
+                    log('\n[Exception] : %s\n'% e)
 
                 # Install
-                log('Installing the update package of version %s ...'% imgage_version_list[i])
-                install_cmd = "sudo python3 /opt/ezblock/" + str(imgage_version_list[i]) + "/" + "ezb-update.py"
+                log('Installing the update package of version %s ...'% updatable_version_list[i])
+                install_cmd = "sudo python3 /opt/ezblock/" + str(updatable_version_list[i]) + "/" + "ezb-update.py"
                 try:
                     cmd_status, cmd_result = run_command(install_cmd)
                     if cmd_status != 0:
@@ -196,25 +168,25 @@ class Ezbupdate(object):
                     else:
                         log('Install completed')
                 except Exception as e:
-                    log('\n[Exception] : %s\n'% e) 
+                    log('\n[Exception] : %s\n'% e)
 
                 # clean install egg-info
                 log("cleaning egg-info ...")
                 clean_cmd = "sudo rm -rf /home/pi/ezb-pi//ezblock/ezblock.egg-info"
                     
-                # Update version information   
-                self.config.set('message', 'version',imgage_version_list[i])
+                # Update version information
+                self.config.set('message', 'version',updatable_version_list[i])
                 try:
                     with open(self.file_address, 'w') as configfile:
                         self.config.write(configfile)
-                    log('Successfully updated version information')    
+                    log('Successfully updated version information')
                 except Exception as e:
-                    log('\n[Exception] : %s\n'% e) 
+                    log('\n[Exception] : %s\n'% e)
                     return False
 
                 # Clean up update package
-                log('Cleaning up the update package of version %s ...'% imgage_version_list[i])
-                clean_cmd = "sudo rm -rf /opt/ezblock/" + str(imgage_version_list[i]) + " " + "/opt/ezblock/"+ str(version_list[i]) + ".zip"
+                log('Cleaning up the update package of version %s ...'% updatable_version_list[i])
+                clean_cmd = "sudo rm -rf /opt/ezblock/" + str(updatable_version_list[i]) + " " + "/opt/ezblock/"+ str(version_list[i]) + ".zip"
                 try:
                     cmd_status, cmd_result = run_command(clean_cmd)
                     if cmd_status != 0:
@@ -223,17 +195,18 @@ class Ezbupdate(object):
                     else:
                         log('Cleaned up successfully')
                 except Exception as e:
-                    log('\n[Exception] : %s\n'% e)    
+                    log('\n[Exception] : %s\n'% e)
 
         # Update completed
         log("Successfully updated. Have fun with it.")
         # Reboot
-        log("Please restart the device for the changes to take effect.")          
+        log("Please restart the device for the changes to take effect.")
 
         return True
 
 if __name__ == "__main__":
     ezb = Ezbupdate()
-    result = ezb.get_status("3.3.10")
+    result = ezb.get_status("3.2.15")
     print('Has update ? : %s'%result)
-    # ezb.update("3.3.10")
+    # result = ezb.update("3.2.15")
+    # print('update result : %s'%result)
