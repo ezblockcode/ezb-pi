7f453575ff840b5336a881682c7b1b319fddc542
Lovmong <1967471283@qq.com>
2024-02-07 10:39:50 +0800

    rm smbus

--- ezblock/ezblock/pwm.py
+++ ezblock/ezblock/pwm.py
@@ -1,4 +1,4 @@
-import smbus, math
+import math
 from .i2c import I2C
 
 timer = [
@@ -35,7 +35,6 @@ class PWM(I2C):
         self._debug("PWM address: {:02X}".format(self.ADDR))
         self.channel = channel
         self.timer = int(channel/4)
-        self.bus = smbus.SMBus(1)
         self._pulse_width = 0
         self._freq = 50
         self.freq(50)
