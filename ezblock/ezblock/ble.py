1c53a7e951c86cad8a81899c802e4e104ca4c413
Lovmong <1967471283@qq.com>
2024-11-01 17:22:48 +0800

    optimzie

--- ezblock/ezblock/ble.py
+++ ezblock/ezblock/ble.py
@@ -5,7 +5,7 @@ import time
 from .filedb import fileDB
 from .basic import _Basic_class
 from .ble_uart import BLE_UART, LOCAL_NAME
-from .utils import  log
+from .utils import  log, run_command
 
 def _log(msg:str, location='BLE', end='\n', flush=False, timestamp=True):
     log(msg, location, end='\n', flush=False, timestamp=True)
@@ -16,10 +16,10 @@ class BLE(_Basic_class):
         super().__init__()
         if not self.is_ble_only():
             _log("Not BLE only, changing config")
-            self.run_command("sudo btmgmt power off")
-            self.run_command("sudo btmgmt le on")
-            self.run_command("sudo btmgmt bredr off")
-            self.run_command("sudo btmgmt power on")
+            run_command("sudo btmgmt power off")
+            run_command("sudo btmgmt le on")
+            run_command("sudo btmgmt bredr off")
+            run_command("sudo btmgmt power on")
             time.sleep(0.5)
             self.is_ble_only()
 
@@ -28,9 +28,9 @@ class BLE(_Basic_class):
 
     def reset(self):
         _log("BLE: reset")
-        self.run_command("sudo btmgmt power off")
+        run_command("sudo btmgmt power off")
         time.sleep(0.5)
-        self.run_command("sudo btmgmt power on")
+        run_command("sudo btmgmt power on")
         time.sleep(0.5)
 
     def read(self, num=None):
@@ -69,7 +69,7 @@ class BLE(_Basic_class):
 
     def is_ble_only(self):
         settings = []
-        _, result = self.run_command("sudo btmgmt info")
+        _, result = run_command("sudo btmgmt info")
         for line in result.split("\n"):
             line = line.strip()
             if line.startswith("current settings: "):
