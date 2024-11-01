42629d8c2f5d6dff418d5ab202a0cbfee1a4875c
Lovmong <1967471283@qq.com>
2024-11-01 17:22:01 +0800

    rm not in use

--- ezblock/tests/adc_raw.py
+++
@@ -1,9 +0,0 @@
-import smbus
-ADDR = 0x14
-
-bus = smbus.SMBus(1)
-bus.write_byte(ADDR, 0x10)
-value_h = bus.read_byte(ADDR)
-value_l = bus.read_byte(ADDR)
-value = (value_h << 8) + value_l
-print(value)
